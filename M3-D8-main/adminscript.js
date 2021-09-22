11f11cf1d3c689b1d541a23de0d50fccfe99d350
Code4Wyatt <44061707+Code4Wyatt@users.noreply.github.com>
2021-09-22 14:30:38 +0100

    Updated, put working working on delete function and style

---
+++ M3-D8-main/adminscript.js
@@ -0,0 +1,82 @@
+const params = new URLSearchParams(location.search)
+const id = params.get('id')
+const url = "https://striveschool-api.herokuapp.com/api/product/"
+
+window.onload = function () {
+
+    if (id) document.querySelector("#page-title").innerHTML = "Edit product"
+    getProductDetails(id)
+
+}
+
+const handleSubmit = async (event) => {
+    event.preventDefault()
+    
+    const url = "https://striveschool-api.herokuapp.com/api/product/"
+
+    const myProduct = {
+        name: document.getElementById("name").value,
+        description: document.getElementById("description").value,
+        brand: document.getElementById("brand").value,
+        imageUrl: document.getElementById("imageUrl").value,
+        price: document.getElementById("price").value,
+
+    }
+
+    try {
+        const response = await fetch(url, {
+            method: "POST",
+            body: JSON.stringify(myProduct), // To convert myProduct from an Object to a String
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTRiMjdjMjRiYjUzZDAwMTViMTlmNzEiLCJpYXQiOjE2MzIzMTUzMzAsImV4cCI6MTYzMzUyNDkzMH0.kdEn_VWem83-DD0gu4hQZit-Om1MxwN1e4XiFcWnbeQ",
+            }
+        })
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+
+async function deleteProduct() {
+      
+      try {
+          const response = await fetch(url + id, {
+              method: 'DELETE',
+              headers: {
+                "Content-Type": "application/json",
+                "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTRiMjdjMjRiYjUzZDAwMTViMTlmNzEiLCJpYXQiOjE2MzIzMTUzMzAsImV4cCI6MTYzMzUyNDkzMH0.kdEn_VWem83-DD0gu4hQZit-Om1MxwN1e4XiFcWnbeQ",
+            } })
+
+        if (!response.ok) throw new Error("Failed to delete product")
+
+        alert("All good - product deleted successfully.")
+        location.assign("index.html")
+      } catch (error) {
+        alert(error.message)
+      }
+    
+}
+    
+async function getProductDetails(id) {
+     
+      try {
+        const response = await fetch(url + id, {
+          headers: {
+                "Content-Type": "application/json",
+                "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTRiMjdjMjRiYjUzZDAwMTViMTlmNzEiLCJpYXQiOjE2MzIzMTUzMzAsImV4cCI6MTYzMzUyNDkzMH0.kdEn_VWem83-DD0gu4hQZit-Om1MxwN1e4XiFcWnbeQ",
+            }
+        })
+        const product = await response.json()
+
+        console.log(product)
+        console.log(Object.keys(product))
+
+        Object.keys(product).forEach(key => {
+          const field = document.querySelector(`#${key}`)
+          if (field) field.value = product[key]
+        })
+      } catch (error) {
+        alert(error)
+      }
+    }
---
+++ M3-D8-main/script.js
@@ -0,0 +1,47 @@
+window.onload = async () => {
+    try {
+        const response = await fetch("https://striveschool-api.herokuapp.com/api/product/", {
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTRiMjdjMjRiYjUzZDAwMTViMTlmNzEiLCJpYXQiOjE2MzIzMTUzMzAsImV4cCI6MTYzMzUyNDkzMH0.kdEn_VWem83-DD0gu4hQZit-Om1MxwN1e4XiFcWnbeQ",
+            }
+        })
+        const fetchedProducts = await response.json()
+        displayProducts(fetchedProducts)
+        //after the fetch, assing the result to a variable called how ever you want, then call displayProducts(theVariableYouCreated)
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+const displayProducts = (products) => {
+    const productSection = document.querySelector(".productSection")
+
+    if (products) {
+        products.forEach((product) => {
+          productSection.innerHTML += `<div class="row">
+            <div class="col-md-4">
+            <div class="card" style="width: 18rem">
+              <div class="row">
+              <img src="${product.imageUrl}" class="card-img-top img-fluid" alt="..." />
+              <div class="card-body">
+                <p class="card-text font-weight-bolder">
+                  ${product.name}
+                </p>
+                <p class="card-text font-weight-bold">
+                  ${product.brand}
+                </p>
+                <p class="card-text">
+                  ${product.description}
+                </p>
+                <a href="#" class="btn btn-primary">${product.price}</a>
+                <a class="btn btn-warning" href="backoffice.html?id=${product._id}">Edit</a>
+              </div>
+              </div>
+            </div>
+          </div>
+          </div>`
+        })
+    }
+
+}
